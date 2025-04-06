2ddb80260d172f3db5e22c1349e2a6984428d476
seyda.yildirim <seyda.yildirim@togg.com.tr>
2025-04-06 22:15:54 +0300

    AssetControllerTest is updated.

--- microservices/asset/src/test/java/com/inghubs/asset/controller/AssetControllerTest.java
+++ microservices/asset/src/test/java/com/inghubs/asset/controller/AssetControllerTest.java
@@ -1,6 +1,9 @@
 package com.inghubs.asset.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inghubs.asset.dto.AssetDto;
+import com.inghubs.asset.dto.CreateAssetRequestDto;
+import com.inghubs.asset.dto.UpdateAssetRequestDto;
 import com.inghubs.asset.model.Asset;
 import com.inghubs.asset.service.AssetService;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,17 +11,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 
 @ActiveProfiles("dev")
 @WebMvcTest(AssetController.class)
@@ -37,6 +46,7 @@ public class AssetControllerTest {
     private final Long customerId = 10L;
     private final String tryAssetName = "TRY";
     private final String aselsAssetName = "ASELS";
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
@@ -74,6 +84,9 @@ public class AssetControllerTest {
                 BigDecimal.valueOf(80)
         );
 
+        objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+
     }
 
     @Test
@@ -82,13 +95,28 @@ public class AssetControllerTest {
         List<AssetDto> assets = Arrays.asList(tryAssetDto, aselsAssetDto);
         when(assetService.getAssetsByCustomerId(customerId)).thenReturn(assets);
 
-
         // When & Then
         mockMvc.perform(get("/api/v1/assets/customer/{customerId}", customerId))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1))
                 .andExpect(jsonPath("$[0].customerId").value(customerId))
                 .andExpect(jsonPath("$[0].assetName").value(tryAssetName))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].customerId").value(customerId))
                 .andExpect(jsonPath("$[1].assetName").value(aselsAssetName));
+
+    }
+
+    @Test
+    void getAssetsByCustomerId_ShouldReturnNoContent_WhenNoAssetsExist() throws Exception {
+
+        Long customerId = 1L;
+
+        when(assetService.getAssetsByCustomerId(customerId)).thenReturn(new ArrayList<>());
+
+        mockMvc.perform(get("/api/v1/assets/customer/{customerId}", customerId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
     }
 
     @Test
@@ -103,6 +131,72 @@ public class AssetControllerTest {
                 .andExpect(jsonPath("$.assetName").value(tryAssetName))
                 .andExpect(jsonPath("$.size").value("1000"))
                 .andExpect(jsonPath("$.usableSize").value("300"));
+    }
+
+    @Test
+    void getAssetByCustomerIdAndAssetName_ShouldReturnNotFound_WhenAssetNotExists() throws Exception {
+
+        Long customerId = 1L;
+        String assetName = "XRP";
+
+        when(assetService.getAssetByCustomerIdAndAssetName(customerId, assetName)).thenReturn(null);
+
+        mockMvc.perform(get("/api/v1/assets/customer/{customerId}/asset/{assetName}", customerId, assetName)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void updateAsset_ShouldReturnUpdatedAsset_WhenAssetExists() throws Exception {
+
+        UpdateAssetRequestDto requestDto = new UpdateAssetRequestDto(1L, 10L,
+                "TRY", BigDecimal.valueOf(100), BigDecimal.valueOf(10) );
+
+
+        when(assetService.updateAsset(any(UpdateAssetRequestDto.class))).thenReturn(tryAssetDto);
+
+        mockMvc.perform(put("/api/v1/assets/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.customerId").value(10))
+                .andExpect(jsonPath("$.assetName").value("TRY"))
+                .andExpect(jsonPath("$.size").value(1000))
+                .andExpect(jsonPath("$.usableSize").value(300));
+    }
+
+    @Test
+    void updateAsset_ShouldReturnNotFound_WhenAssetNotExists() throws Exception {
+
+        UpdateAssetRequestDto requestDto = new UpdateAssetRequestDto(999L, 10L,
+                "TRY", BigDecimal.valueOf(100), BigDecimal.valueOf(10) );
+
+        when(assetService.updateAsset(any(UpdateAssetRequestDto.class))).thenReturn(null);
+
+        mockMvc.perform(put("/api/v1/assets/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void createAsset_ShouldReturnCreatedAsset() throws Exception {
+
+        CreateAssetRequestDto requestDto = new CreateAssetRequestDto(aselsAssetDto.customerId(),aselsAssetName,
+                aselsAssetDto.size(), aselsAssetDto.usableSize());
+
+        when(assetService.createAsset(any(CreateAssetRequestDto.class))).thenReturn(aselsAssetDto);
+
+        mockMvc.perform(post("/api/v1/assets/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(2))
+                .andExpect(jsonPath("$.customerId").value(10))
+                .andExpect(jsonPath("$.assetName").value("ASELS"))
+                .andExpect(jsonPath("$.size").value(100))
+                .andExpect(jsonPath("$.usableSize").value(80));
     }
 
 }
